3b6b637c681b010dae1f654bf75e9da00adf4f47
khollmes <101499108+khollmes@users.noreply.github.com>
2023-07-20 08:49:46 +0200

    Update eth_mnist.py

--- examples/mnist/eth_mnist.py
+++ examples/mnist/eth_mnist.py
@@ -97,6 +97,9 @@ network = DiehlAndCook2015(
     norm=78.4,
     theta_plus=theta_plus,
     inpt_shape=(1, 28, 28),
+    nu=[1e-5,1e-5], # not sure if I should be doing that
+    wmin = 20e-6,
+    wmax = 40e-6,
 )
 
 # Directs network to GPU
