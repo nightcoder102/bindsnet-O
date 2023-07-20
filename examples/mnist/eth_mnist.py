b302de6c0464a7358bddb517379600a2f1c10692
khollmes <101499108+khollmes@users.noreply.github.com>
2023-07-20 11:15:24 +0200

    Update eth_mnist.py

--- examples/mnist/eth_mnist.py
+++ examples/mnist/eth_mnist.py
@@ -97,7 +97,7 @@ network = DiehlAndCook2015(
     norm=78.4,
     theta_plus=theta_plus,
     inpt_shape=(1, 28, 28),
-    nu=[1e-2,1e-2], # not sure if I should be doing that
+    nu=[1e-3,1e-3], # not sure if I should be doing that
     wmin = 0,
     wmax = 1,
 )
