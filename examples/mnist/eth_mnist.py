6adbfd9f5bb436270aae374b26f9620de59f1b31
khollmes <101499108+khollmes@users.noreply.github.com>
2023-07-24 14:58:15 +0200

    Update eth_mnist.py

--- examples/mnist/eth_mnist.py
+++ examples/mnist/eth_mnist.py
@@ -75,7 +75,7 @@ standard_deviation=0.1
 nu = [1e-4,1e-2] #[1e-4,1e-2]
 print(f'nu: {nu}')
 if standard_deviation>0:
-    tau_pre = torch.normal(mean=tau_pre,std=tau_pre*self.standard_deviation,n_neurons)
+    tau_pre = torch.normal(mean=tau_pre,std=tau_pre*self.standard_deviation,size= (n_neurons,))
     print(tau_pre)
 # Sets up Gpu use
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
