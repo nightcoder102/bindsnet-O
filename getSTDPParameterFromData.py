46eb900b736785e2b693afa3f6723523b2535098
khollmes <101499108+khollmes@users.noreply.github.com>
2023-07-26 14:02:11 +0200

    Add files via upload

---
+++ getSTDPParameterFromData.py
@@ -0,0 +1,166 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
+from scipy.signal import savgol_filter
+
+
+def detectChangeOfPulse(p,tol=8e-6):
+    indicesOfPulse= []
+    sizePN = len(p)
+    for i in range (sizePN):
+        j = i + 1
+        if j< sizePN-1:
+            if abs(p[i]-p[j])>tol:
+                indicesOfPulse.append(j)
+    return indicesOfPulse
+
+def expF(x,a,b,c,d):
+    # the function that I want is a+b*exp(cx).
+    # I add another parameter as a trick to make sure the optimisation converge (give a good result).
+    # it's not neccessary.
+    return a+b*np.exp(c*x+d)
+
+def calculate_derivative(y, h=1):
+    n = len(y)
+    derivative = []
+
+    for i in range(n):
+        if i == 0:  # Forward difference
+            dy = (y[i + 1] - y[i]) / h
+        elif i == n - 1:  # Backward difference
+            dy = (y[i] - y[i - 1]) / h
+        else:  # Average of upper and lower differences
+            dy = (y[i + 1] - y[i - 1]) / (2 * h)
+
+        derivative.append(dy)
+
+    return derivative
+def isPotentiation(y,dp=1):
+    
+    # derive the data and get the sign of the derivative if positive potentiation if negative depression.
+    #only need a few data point. 
+    # because the function need to be strictly monotous, eitheir decreasing or increasing, we should only need 2 points.
+    #BUT because of the sinusoidal signal we have to take more.
+    dy = calculate_derivative(y,dp)
+    return np.mean(dy) > 0
+    
+
+def testEq(f,x,y,p=[]):
+    m =100000
+    popt =[]
+    if len(p)!=0:
+        popt,_ = curve_fit(f,x,y,p0=p,maxfev=m,method='lm')
+    else:
+        popt,_ = curve_fit(f,x,y,maxfev=m,method='lm')
+    ypred = f(x,*popt)
+    r2=r2_score(y,ypred)
+    return (ypred,r2,popt)
+
+
+
+def get_STDP_param_from_data(dir_path = os.path.expanduser("~/data"),pn='Pulse number', cn= 'Conductance',
+            reduceDataSize = 15,filterOn=True,useLinearRegressionMethod= True):
+    # loop over all files in the directory
+    taupreList = []
+    taupostList =[]
+    ApreList =[]
+    ApostList = []
+    g_mins = []
+    g_maxs =[]
+    names= []
+    for file_name in os.listdir(dir_path):
+        print(f'get parameter of Fit for {file_name}')
+        # check if the file is an xls file
+        if file_name.endswith('.xls') or file_name.endswith('.xlsx'):
+            # read the xls file
+            file_path = os.path.join(dir_path, file_name) # get the path of the file
+
+            filename=os.path.splitext(file_name)[0]
+            df = pd.read_excel(file_path) # read the file and store it in the dataframe
+            x = np.array(df[pn])
+            y = np.array(df[cn])
+            changesOfPulse = detectChangeOfPulse(y) # get the pulse limit between depreciation and potentiation.
+            potdep =  np.split(y, changesOfPulse)
+            # set the size
+            if reduceDataSize > 0:
+                x = x[:reduceDataSize]
+                for e in range(len(potdep)):
+                    potdep[e] = potdep[e][:reduceDataSize]
+            potentiation = isPotentiation(potdep[0])
+            linear_regression_gmin = 20e-6
+            linear_regression_gmax = 40e-6
+            if useLinearRegressionMethod:
+                print()
+                print("You are using the linear regression method as we derive the data first gmin and gmax cannot be determine you have to enter them yourself gmin is the minimum value of the conductance respectively gmax is the maximum")
+                print("then change the value of the parameter linear_regression_gmin and linear_regression_gmax to the value you see.")
+                print(f"linear_regression_gmin: {linear_regression_gmin} S, and linear_regression_gmax: {linear_regression_gmax} S")
+                print()
+            A_post = []
+            A_pre = []
+            tau_post = []
+            tau_pre = []
+            g_max = []
+            g_min =[]
+            if filterOn :
+                print('filter is ON you have to set the parameter of filterParameter.')
+                print('it will determine how much the data are smoothed.')
+                print('the closer filterParameter is to the value of len(potdep[e]) the smoother the curve is')
+                for e in range(len(potdep)):
+                    filterParameter= int(2*len(potdep[e])/3)
+                    potdep[e] = savgol_filter(potdep[e], filterParameter, 2)
+            for e in potdep:
+                #Fit the data 
+                if useLinearRegressionMethod:
+                    ys = np.log(np.abs(calculate_derivative(e)))
+                    a, b = np.polyfit(x,ys, 1)
+                    if potentiation:
+                        A_post.append(np.exp(b))
+                        tau_post.append(-1/a)
+                        g_min.append(linear_regression_gmin)
+                    else:
+                        A_pre.append(np.exp(b))
+                        tau_pre.append(-1/a)
+                        g_max.append(linear_regression_gmax)
+                else: 
+                    p = [4e-5,(1e-6 if potentiation else -1e-6),-1,100]
+                    _,r2,param =testEq(expF,x,e,p)
+                    if potentiation:
+                        A_post.append(param[0])
+                        tau_post.append(param[1])
+                        g_min.append(param[2])
+                    else:
+                        A_pre.append(param[0])
+                        tau_pre.append(param[1])
+                        g_max.append(param[2])
+                potentiation= not potentiation
+
+            g_min = np.mean(g_min)
+            g_max = np.mean(g_max)
+            tau_pre = np.mean(tau_pre)
+            tau_post = np.mean(tau_post)
+            A_pre = np.mean(A_pre)
+            A_post = np.mean(A_post)
+
+
+
+            g_mins.append(g_min)
+            g_maxs.append(g_max)
+            taupreList.append(tau_pre)
+            taupostList.append(tau_post)
+            ApostList.append(A_post)
+            ApreList.append(A_pre)
+            names.append(filename)         
+            print('stdp pre from depression: {:.2E} + {:.2E} * exp(-x/{:.2f})'.format(g_min,A_pre,tau_pre))
+            print('stdp post from potentiation: {:.2E} {:.2E} * exp(-x/{:.2f})'.format(g_max,A_post,tau_post))
+    return {'g_min': g_mins,
+             'g_max':g_maxs,
+             'tau_pre':taupreList,
+             'tau_post':taupostList,
+             'A_post':ApostList,
+             'A_pre': ApreList,
+             'filenames':names}
+
+
---
+++ snn simulation.py
@@ -0,0 +1,127 @@
+
+from bindsnet.getAccuracy import getAccuracy
+from getSTDPParameterFromData import get_STDP_param_from_data
+import os
+import time
+from math import isnan
+
+def create_directory(directory_name='logs'):
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+        print(f"Directory '{directory_name}' created.")
+    else:
+        print(f"Directory '{directory_name}' already exists.")
+
+
+
+def write_in_directory(directory_name, file_name, content):
+    # Write a file with the specified content inside the directory
+    file_path = os.path.join(directory_name, file_name)
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+#hyperparameter to control the data 
+n_neurons = 100
+n_epochs = 1
+n_test = 10000
+n_train = 60000
+exc = 22.5
+inh=120
+theta_plus=0.05
+time=250
+progress_interval=250
+update_interval =4000
+
+
+#parameter controlling how the simulation works
+
+train = True #should train the network or use one already trained?
+plot=False # should you plot everything ? Take more time to do one simulation but it's pretty.
+gpu = True #should you use GPU.
+
+
+
+
+params = get_STDP_param_from_data()
+"""
+get_STDP_param_from_data will get all the data from the directory in the path dirpath.
+This means that if you have several set of data in different file you can just put all of them
+"""
+#params issued from the fit
+tau_pres = params['tau_pre']
+tau_posts = params['tau_post']
+A_pres = params['A_pre']
+A_posts = params['A_post']
+g_mins = params['g_min']
+g_maxs = params['g_max']
+names = params['filenames']
+
+print(params)
+
+
+# Create the directory if it doesn't exist
+create_directory()
+
+# Write the logs inside a file to save the  inside the directory
+
+
+
+
+
+for i in range(len(tau_pres)):
+    tau_pre=tau_pres[i]
+    tau_post = tau_posts[i]
+    A_pre= A_pres[i]
+    A_post =A_posts[i]
+    g_min = g_mins[i]
+    g_max = g_maxs[i]
+    name = names[i]
+    if isnan(tau_pre) and isnan(tau_post) and isnan(A_pre) and isnan(A_post) and isnan(g_min) and isnan(g_max):
+        print('error found NaN in the parameter')
+        break
+    print(f'get accuracy for the fit issued from the file: {name}')
+    
+    accuracy = getAccuracy(n_neurons = 100,
+                    n_epochs = 1, 
+                    n_test = 10000,
+                    n_train = 60000, 
+                    exc = 22.5,
+                    inh=120,
+                    theta_plus=0.05,
+                    time=250,
+                    progress_interval=250,
+                    update_interval =4000,
+                    train = train,
+                    plot=plot,
+                    gpu = gpu,
+                    tau_pre = tau_pre,
+                    tau_post=tau_post,
+                    A_pre = A_pre,
+                    A_post = A_post,
+                    g_max = g_max,
+                    g_min =g_min,
+                    standard_deviation = 0.0,
+                    nu_pre = 1e-4,
+                    nu_post=1e-2)
+    file_content = f"Accuracy for file: {name}\n"
+    file_content += f'''n_neurons = {n_neurons}\n
+                    n_train = {n_train}\n
+                    n_epochs = {n_epochs}\n
+                    n_test = {n_test}\n
+                    exc = {exc}\n
+                    inh={inh}\n
+                    theta_plus={theta_plus}\n
+                    time={time}\n
+                    progress_interval={progress_interval}\n
+                    update_interval ={update_interval}\n'''
+    file_content += '\n\n'
+    all_activity_accuracy = accuracy["all"] / n_test
+    propotion_activity_accuracy = accuracy["proportion"] / n_test
+    file_content += f'All activity accuracy: {all_activity_accuracy}\n'
+    file_content += f'Propotion activity accuracy : {propotion_activity_accuracy}\n'
+    log_name = f'accuracy_for_file_{name}.txt'
+
+    write_in_directory(unique_directory_name, log_name, file_content)
+    
