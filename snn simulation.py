671da16eb5088b4f1c09ebad5e472738ae171ee5
khollmes <101499108+khollmes@users.noreply.github.com>
2023-07-27 10:08:56 +0200

    Update snn simulation.py

--- snn simulation.py
+++ snn simulation.py
@@ -45,7 +45,8 @@ gpu = True #should you use GPU.
 
 
 
-params = get_STDP_param_from_data()
+params = get_STDP_param_from_data(dir_path = os.path.expanduser("~/data"),pn='Pulse number', cn= 'Conductance',
+            reduceDataSize = 15,filterOn=True,useLinearRegressionMethod= False)
 """
 get_STDP_param_from_data will get all the data from the directory in the path dirpath.
 This means that if you have several set of data in different file you can just put all of them
